9db911756005f1208aaf8b73ed4c57857901bf74
Jillian Burgess <jillburg7@gmail.com>
2014-03-17 17:24:04 -0400

    Updated to most recently released Foundation (v5.2.1)
    Now accurately reflects the content & code on Foundation Docs website
    (http://foundation.zurb.com/docs/)

--- js/script.js
+++ js/script.js
@@ -6,8 +6,24 @@
 // var selected_file = $('#input').get(0).files[0];
 
 $("#course-name").click(function() { //anonymous function 
-  console.log(Foundation.utils.random_str(6));
+  console.log(Foundation.utils.random_str(1));
 });
 
-
-  
+/*
+var request;
+if (window.XMLHttpRequest) {
+	request = new XMLHttpRequest();
+} else {
+	request = new ActiveXObject("Microsoft.XMLHTTP");
+}
+request.open('GET', 'data.txt');
+request.onreadystatechange = function() {
+	if ((request.readyState===4) && (request.status===200)) {
+		console.log(request);
+		// document.writeln(request.responseText);
+		var modify = document.getElementById("course-name");
+		modify.innerHTML =request.responseText;
+	}
+}
+request.send();
+*/
