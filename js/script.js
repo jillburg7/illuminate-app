c2260b155d925e4d53c6ada638dc4ef3a9552c6f
Jillian Burgess <jillburg7@gmail.com>
2014-03-17 08:58:06 -0400

    required form inputs for signup & login pages

--- js/script.js
+++ js/script.js
@@ -2,3 +2,12 @@
 // 
 
 // Foundation.utils.random_str(6);
+
+// var selected_file = $('#input').get(0).files[0];
+
+$("#course-name").click(function() { //anonymous function 
+  console.log(Foundation.utils.random_str(6));
+});
+
+
+  
