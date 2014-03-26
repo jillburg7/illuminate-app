1088c29c1b91085afd82cd6a520a5bbcf0e44a3b
Jillian Burgess <jillburg7@gmail.com>
2014-03-26 11:41:04 -0400

    added working php for signup page

---
+++ insert.php
@@ -0,0 +1,21 @@
+<?php
+$con=mysqli_connect("localhost","root","root","Illuminate");
+// Check connection
+if (mysqli_connect_errno())
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+
+$sql="INSERT INTO CreateLogin (userID, email, password, type, firstName, lastName, studentID)
+
+VALUES
+(null,'$_POST[email] ', '$_POST[password1]', '$_POST[userType]','$_POST[firstname]','$_POST[lastname]', $_POST[studentID])";
+
+if (!mysqli_query($con,$sql))
+  {
+  die('Error: ' . mysqli_error($con));
+  }
+echo "1 record added";
+
+mysqli_close($con);
+?>
--- js/script.js
+++ js/script.js
@@ -1,16 +1,21 @@
 //login & signup form handling
 
-$("#register").click(function() {
-	if ($("#password1").val() === $("#password2").val()) {
-		//passwords match
-		console.info("passwords match");
-	}
-	else {
-		//clear fields & give focus to pw field
+$("#register").submit(function() {
+	if ($("#password1").val() != $("#password2").val()) {
+		 & give focus to pw field
 		$("#password1").val('');
 		$("#password2").val('');
     	$('#password1').focus();
     	//should probably create a more formal error...
 		console.info("passwords do NOT match, try again");
 	}
+	else {
+		//passwords match
+		console.info("passwords match");//clear fields
+		var pw = $("#password1").val();
+		var digit = "0123456789";
+		var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		// if (pw.contains() ) {}
+	}
 });
+
