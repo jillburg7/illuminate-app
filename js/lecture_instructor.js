2e838c1a02ba6effb6e1395ddb9779142ce626e4
Jillian Burgess <jillburg7@gmail.com>
2014-04-05 21:46:57 -0400

    added some reg expr filtering to mock student question data; added status radios to the 'incoming questions' instructor lecture interface - only visible in the large view as of now

--- js/lecture_instructor.js
+++ js/lecture_instructor.js
@@ -1,8 +1,7 @@
 
-//lecture.html stuff below
+//Instructor lecture.html stuff below
 //Use data generator to generate student names and questions they ask and then export as 
 //JSON -> which will then be used in the JavaScript regular expression matcher/aggregator
-
 
 /** 
 * The Idea:
@@ -12,29 +11,33 @@
 * -> [Lastly, clicking into view -> modify status options (radios)]
 */
 $(document).ready(function() {
-  $.getJSON('../test-data.json', function(data) {
-    var output = '<ul class="incoming">';
-    var num = 0;
-    $.each(data, function(key, val) {
-      // if ((val.name.search(myExp) != -1) || (val.bio.search(myExp) != -1)) {
-        output += '<li id="q'+ num +'">';
-        output += '<h6>'+ val.name +'</h6>';
-        output += '<h4><small>'+ val.question +'</small></h4>';
-        output += '</li>';
-        num++;
-      // }
-    });
-    output += '</ul>';
-    $('#panel12').html(output);
-    notificationBadge(num);
-  }); //getJSON
-  /* toggles displaying the current set of questions */
-  $("#notification-count").click(function () {
-    // if()
-    $("#msgs > dd:first > div").toggle();
-    $("#msgs > dd:first > div").toggleClass("active");
-    $("#msgs > dd:first").toggleClass("active");
-  }); 
+	$.getJSON('../test-data.json', function(data) {
+		var output = '<ul class="incoming">';
+		var num = 0;
+		var re = new RegExp("fly");
+		$.each(data, function(key, val) {
+			// if ((val.name.search(myExp) != -1) || (val.bio.search(myExp) != -1)) {}
+			if (val.question.search(re) != -1) {
+				output += '<li id="q'+ num +'">';
+				output += '<h6>'+ val.name +'</h6>';
+				output += '<h4><small>'+ val.question +'</small></h4>';
+				output += '<div class="statuschanger" ><input type="radio" name="status'+ num +'" value="default" checked /><label for="default">Not answered</label><br/><input type="radio" name="status'+ num +'" value="save"/><label for="save">Save for later</label><br/><input type="radio" name="status'+ num +'" value="answered"/><label for="answered">Answered</label></div>' + '</li>';
+				num++;
+			}
+		});
+		output += '</ul>';
+		$('#panel12').html(output);
+		notificationBadge(num);
+	}); //getJSON
+	/* toggles displaying the current set of questions */
+	$("#notification-count").click(function () {
+		// if()
+		$("#msgs > dd:first > div").toggle();
+		$("#msgs > dd:first > div").toggleClass("active");
+		$("#msgs > dd:first").toggleClass("active");
+		$(".incoming > li").children(".statuschanger").removeClass("active");
+		status();
+	});
 });
 
 /*
@@ -43,6 +46,18 @@ $(document).ready(function() {
 * marked as opened/read)
 */
 function notificationBadge(num) {
-  $("#notification-count").append('   <span class="label round">' + num + '</span>');
+	$("#notification-count").append('   <span class="label round">' + num + '</span>');
 } //notificationBadge
 
+function divvy() {
+	var re = new RegExp("weather");
+}
+
+function status() {
+	var questions = $(".incoming > li h6, .incoming > li h4");
+	questions.click(function () {
+		if (!$(this).siblings(".statuschanger").hasClass("active"))
+			$(".incoming > li").children(".statuschanger").removeClass("active");
+		$(this).siblings(".statuschanger").toggleClass("active");
+	});
+}
