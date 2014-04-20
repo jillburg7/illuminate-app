b409912ea471f37808890fc2f9d545f9a53e349b
Jillian Burgess <jillburg7@gmail.com>
2014-04-20 13:33:33 -0400

    semi-responsive lecture content; doesn't support adjusting browsers size manually or device orientation changes except for  when other page elements that effect it's viewport...(when notifications are shown or when it first loads)

--- js/lecture_instructor.js
+++ js/lecture_instructor.js
@@ -2,7 +2,7 @@
 //Instructor lecture.html stuff below
 //Use data generator to generate student names and questions they ask and then export as 
 //JSON -> which will then be used in the JavaScript regular expression matcher/aggregator
-
+var doc;// = PDFJS.getDocument('../files/ln025.pdf');
 /** 
 * The Idea:
 * -> Query db for all students questions (@ some specified rate...!)
@@ -11,6 +11,9 @@
 * -> [Lastly, clicking into view -> modify status options (radios)]
 */
 $(document).ready(function() {
+	doc = PDFJS.getDocument('../files/ln025.pdf');
+	prepareDocument(doc);
+
 	$.getJSON('../test-data.json', function(data) {
 		var output = '<ul class="incoming">';
 		var num = 0;
@@ -37,27 +40,29 @@ $(document).ready(function() {
 
 	/* toggles displaying the current set of questions */
 	$("#notification-count").click(function () {
-		$("#slider").toggle();
-  	//$("#lecture").toggleClass("small-12 medium-9 large-9 columns");
+		// $("#slider").toggle();
+		//$("#lecture").toggleClass("small-12 medium-9 large-9 columns");
 
 		if($("#lecture").hasClass("medium-12 large-12")) {
 			$("#lecture").removeClass("medium-12 large-12").addClass("medium-9 large-9");
 			$("#slider").removeClass('animated slideOutRight').addClass('animated slideInRight');
+			calculateScale();
+			$("#slider").show();
 		}
 		else if ($("#lecture").hasClass("medium-9 large-9")) {
 			$("#lecture").removeClass("medium-9 large-9").addClass("medium-12 large-12");
-		$("#slider").removeClass('animated slideInRight').addClass('animated slideOutRight');
+			$("#slider").removeClass('animated slideInRight');
+			calculateScale();
+			$("#slider").css("display", "none");
 		}
 		$("#slider").toggleClass("small-12 medium-3 large-3 columns question-panel");
-		// $("#slider").toggleClass('animated slideInRight');
 
-	
 
-		// $("#msgs > dd:first > div").toggle();
-		// $("#msgs > dd:first > div").toggleClass("active");
-		// $("#msgs > dd:first").toggleClass("active");
-		// $(".incoming > li").children(".statuschanger").removeClass("active");
-		// status();
+		$("#msgs > dd:first > div").toggle();
+		$("#msgs > dd:first > div").toggleClass("active");
+		$("#msgs > dd:first").toggleClass("active");
+		$(".incoming > li").children(".statuschanger").removeClass("active");
+		status();
 	});
 	$("#msgs > dd:first > a").click(function () {
 		$("#msgs > dd:first > div").toggle();
@@ -66,13 +71,16 @@ $(document).ready(function() {
 		$(".incoming > li").children(".statuschanger").removeClass("active");
 		status();
 	});
+
+	$("#save").click(function() { pages(); });
 });
 
+
 /*
-* Applies a number count & notification badge to display the current number
-* of 'unopened' notifications (once clicked, all current notifications are 
-* marked as opened/read)
-*/
+	* Applies a number count & notification badge to display the current number
+	* of 'unopened' notifications (once clicked, all current notifications are 
+	* marked as opened/read)
+	*/
 function notificationBadge(num) {
 	$("#notification-count").append('   <span class="label round">' + num + '</span>');
 } //notificationBadge
@@ -87,5 +95,89 @@ function status() {
 		if (!$(this).siblings(".statuschanger").hasClass("active"))
 			$(".incoming > li").children(".statuschanger").removeClass("active");
 		$(this).siblings(".statuschanger").toggleClass("active");
+		// $(this).siblings(".statuschanger").slideToggle();
+	});
+}
+
+function prepareDocument(doc) {
+	//
+	// See README for overview
+	//
+	'use strict';
+	//
+	// Fetch the PDF document from the URL using promises
+	//
+	doc.then(function(pdf) {
+		// Using promise to fetch the page
+		pdf.getPage(1).then(function(page) {
+			// var scale = 1.2;//1.5;
+			// var viewport = page.getViewport(scale);
+			// console.log(viewport);
+
+			var desiredWidth = $("#lecture").width();
+			var viewport = page.getViewport(1);
+			var scale = desiredWidth / viewport.width;
+			var scaledViewport = page.getViewport(scale);
+			console.log(scale);
+			// console.log(scaledViewport);
+			viewport = scaledViewport;
+
+			//
+			// Prepare canvas using PDF page dimensions
+			//
+			var canvas = document.getElementById('the-canvas');
+			var context = canvas.getContext('2d');
+			canvas.height = viewport.height;
+			canvas.width = viewport.width;
+
+			//
+			// Render PDF page into canvas context
+			//
+			var renderContext = {
+				canvasContext: context,
+				viewport: viewport
+			};
+			page.render(renderContext);
+		});
+	});
+}
+
+function calculateScale() {
+	doc.then(function(pdf) {
+		// Using promise to fetch the page
+		pdf.getPage(1).then(function(page) {
+
+			var desiredWidth = $("#lecture").width();
+			var viewport = page.getViewport(1);
+			var scale = desiredWidth / viewport.width;
+			var scaledViewport = page.getViewport(scale);
+			console.log(scale);
+			console.log(scaledViewport);
+			viewport = scaledViewport;
+
+			//
+			// Prepare canvas using PDF page dimensions
+			//
+			var canvas = document.getElementById('the-canvas');
+			var context = canvas.getContext('2d');
+			canvas.height = viewport.height;
+			canvas.width = viewport.width;
+
+			//
+			// Render PDF page into canvas context
+			//
+			var renderContext = {
+				canvasContext: context,
+				viewport: viewport
+			};
+			page.render(renderContext);
+		});
+	});
+}
+
+
+function pages() {
+	doc.then(function(pdf) {
+		console.log(pdf.numPages);
 	});
 }
